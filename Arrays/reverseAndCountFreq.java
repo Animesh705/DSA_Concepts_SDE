3a6a0ad1445622acf10467b79c6afe70793504b4
Animesh Anand <animeshanand705@gmail.com>
2025-11-04 18:08:18 +0530

    4th Nov: reversing the array , and counting the frequency of each element

--- Arrays/reverseAndCountFreq.java
+++ Arrays/reverseAndCountFreq.java
@@ -1,6 +1,6 @@
 package Arrays;
 import java.util.*;
-public class reverse {
+public class reverseAndCountFreq {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter value of n : ");
@@ -11,11 +11,32 @@ public class reverse {
             arr[i] = sc.nextInt();
         }
         rev(arr);
+        countFreq(arr,n);
     }
     static void rev( int[] arr){
         for (int i = arr.length-1; i >= 0; i--) {
             System.out.print(arr[i]+" ");
         }
         return;
+    }
+    public static void countFreq(int arr[], int n) {
+        boolean visited[] = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+
+            // Skip this element if already processed
+            if (visited[i] == true)
+                continue;
+
+            // Count frequency
+            int count = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    visited[j] = true;
+                    count++;
+                }
+            }
+            System.out.println(arr[i] + " " + count);
+        }
     }
 }
