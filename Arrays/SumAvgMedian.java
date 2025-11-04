6c2f1cd0a35e5d23e114a8914ca5a688e75026c2
Animesh Anand <animeshanand705@gmail.com>
2025-11-04 18:59:17 +0530

    4th Nov: Sum, Average and median of array

--- Arrays/SumAvgMedian.java
+++ Arrays/SumAvgMedian.java
@@ -1,4 +1,34 @@
 package Arrays;
-
+import java.util.*;
 public class SumAvgMedian {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value of n : ");
+        int n = sc.nextInt();
+        System.out.print("Enter value of each element of array : ");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        math(arr);
+    }
+    public static void math(int[] arr){
+        int sum=0;
+        double avg=0;
+        int median=0;
+        for(int j=0;j<arr.length;j++){
+           sum= sum +arr[j];
+        }
+        avg= sum/arr.length;
+        if(arr.length%2==0) {
+            median = arr.length / 2;
+        }
+                else {
+            median = arr.length / 2 + 1;
+        }
+        System.out.println("sum of the array is : "+sum);
+        System.out.println("average is : "+avg);
+        System.out.println("median is : "+median);
+
+    }
 }
