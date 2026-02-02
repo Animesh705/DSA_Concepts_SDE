f5789273b6c09084d113dbb2736ac411de41761a
Animesh Anand <animeshanand705@gmail.com>
2026-02-02 11:35:38 +0530

    2/2/26 --> InOrder Traversal (left-root-right)

--- BinaryTree/InOrderTraversal.java
+++ BinaryTree/InOrderTraversal.java
@@ -1,4 +1,41 @@
 package BinaryTree;
+import java.util.*;
+
 
 public class InOrderTraversal {
+        public void inorderTraversal(Node root,java.util.List<Integer> inorder) {
+            if(root==null) return;
+
+            inorderTraversal(root.left,inorder);
+            inorder.add(root.data);
+            inorderTraversal(root.right,inorder);
+        }
+        public List<Integer> inorder(Node root) {
+            List<Integer> list=new ArrayList<>();
+            inorderTraversal(root,list);
+            return list;
+        }
+    public static void main(String[] args) {
+
+        // Creating a sample binary tree
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        // Getting preorder traversal
+        InOrderTraversal sol = new InOrderTraversal();
+        List<Integer> result = sol.inorder(root);
+
+        // Displaying the preorder traversal result
+        System.out.print("inorder Traversal: ");
+        // Output each value in the
+        // preorder traversal result
+        for (int val : result) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+
 }
