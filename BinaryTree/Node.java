0efab81430a501ee5506f4f1f5edb0201db10403
Animesh Anand <animeshanand705@gmail.com>
2026-02-02 11:34:36 +0530

    2/2/26 --> Node class

---
+++ BinaryTree/Node.java
@@ -0,0 +1,15 @@
+package BinaryTree;
+import java.util.*;
+public class Node {
+    int data;
+    BinaryTree.Node left;
+    BinaryTree.Node right;
+
+    // Constructor to initialize
+    // the node with a value
+    Node(int val) {
+        data = val;
+        left = null;
+        right = null;
+    }
+}
