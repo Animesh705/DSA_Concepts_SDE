b17dd5aac326ee29ab1f6d9c06f00b3336ee30fd
Animesh Anand <animeshanand705@gmail.com>
2026-04-30 06:14:47 +0530

    dp56- Given an n * m matrix of ones and zeros, return how many square submatrices have all ones.

---
+++ DP/dp56_count_square.java
@@ -0,0 +1,44 @@
+package DP;
+import java.util.*;
+public class dp56_count_square {
+    public int countSquares(int[][] matrix) {
+        int n= matrix.length;
+        int m=matrix[0].length;
+        int[][] dp= new int[n][m];
+        //copy first row and col
+        for(int i=0;i<n;i++)  dp[i][0]=matrix[i][0];
+        for(int j=0;j<m;j++) dp[0][j]=matrix[0][j];
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][j]==0) dp[i][j]=0;
+                else{
+                    dp[i][j]= Math.min(dp[i-1][j],Math.min(dp[i-1][j-1],dp[i][j-1])) +1;
+                }
+            }
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                sum += dp[i][j];
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        // Input matrix
+        int[][] arr = {
+                {0, 1, 1, 1},
+                {1, 1, 1, 1},
+                {0, 1, 1, 1}
+        };
+        // Create Solution object
+       dp56_count_square obj = new dp56_count_square();
+
+        // Call function
+        int squares = obj.countSquares(arr);
+
+        // Print the result
+        System.out.println("The number of squares: " + squares);
+    }
+}
